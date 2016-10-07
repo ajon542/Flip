7211919712f81c7fbe8c4d27d5256f99d8dabbfd
Andrew Jones <ajon542@gmail.com>
2016-10-07 16:48:45 -0700

    Removed unity vs and added card sample

---
+++ CardShuffler/Assets/Scripts/Game/Card.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Card : MonoBehaviour
+{
+    public Texture2D face;
+    public GameObject frontFace;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Debug.Log("change");
+            frontFace.GetComponent<MeshRenderer>().sharedMaterial.SetTexture("_MainTex", face);
+        }
+    }
+}
