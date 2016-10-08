2a8060d3ca09e992ad7b3ad6c37da32e80a5d8f0
Andrew Jones <ajon542@gmail.com>
2016-10-08 10:44:37 -0700

    Added history

--- CardShuffler/Assets/Scripts/Game/Views/HistoryView.cs
+++ CardShuffler/Assets/Scripts/Game/Views/HistoryView.cs
@@ -1,21 +1,36 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections.Generic;
 
 /// <summary>
 /// This view keeps track of and displays the game status.
 /// </summary>
-public class GameStatusView : IGameView
+public class HistoryView : IGameView
 {
     //// TODO: Game start
     //// TODO: Game in progress
     //// TODO: Game over - no more cards left in deck
     //// TODO: Game history - previous cards dealt
 
+
+    /// <summary>
+    /// This provides the parent object to be able to attach the runtime generated inventory list.
+    /// </summary>
+    [SerializeField]
+    private GameObject inventoryListParent;
+
+    /// <summary>
+    /// The button prefab to be generated. This represents a single item in the store.
+    /// </summary>
+    [SerializeField]
+    private GameObject listItemPrefab;
+
     private List<CardModel> previousCards;
 
     public override void Initialize(Presenter presenter)
     {
         previousCards = new List<CardModel>();
+        base.Initialize(presenter);
     }
 
     /// <summary>
@@ -26,6 +41,12 @@ public class GameStatusView : IGameView
     private void ReceiveCardDealtMsg(CardDealtMsg msg)
     {
         previousCards.Add(msg.Card);
+
+        GameObject listItem = (GameObject)Instantiate(listItemPrefab);
+
+        listItem.GetComponentInChildren<Text>().text = msg.Card.ToString();
+
+        listItem.transform.SetParent(inventoryListParent.transform);
     }
 
     /// <summary>
